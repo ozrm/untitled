78f460da630c0a7692c0a5aa56a7e00d3abac234
ozrm <130150112@stu.hit.edu.cn>
2019-09-05 23:54:48 +0800

    yanghuita

--- src/partfive/YangHuita.java
+++ src/partfive/YangHuita.java
@@ -1,4 +1,34 @@
 package partfive;
 
-class YangHuita {
+public class YangHuita {
+    public static void main(String[] args) {
+        int levelCount = 5;
+        int[][] yanghui = new int[levelCount][0];
+        for (int i = 0; i < levelCount; i++) {
+            yanghui[i] = new int[i + 1];
+            for (int j = 0; j <= i; j++) {
+                if (i == 0) {
+                    yanghui[i][j] = 1;
+                } else {
+                    int value = 0;
+                    if (j >= 1) {
+                        value += yanghui[i - 1][j - 1];
+                    }
+                    if (j < i) {
+                        value += yanghui[i - 1][j];
+                    }
+                    yanghui[i][j] = value;
+                }
+            }
+        }
+        for (int i = 0; i < levelCount; i++) {
+            for (int j = 0; j < levelCount - i - 1; j++) {
+                System.out.print("\t");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print(yanghui[i][j] + "\t\t");
+            }
+            System.out.println();
+        }
+    }
 }
